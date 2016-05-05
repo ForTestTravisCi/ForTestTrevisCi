c804ba1cbc687fb13904ae2a21db0f3f234c6f83
Aesfirius <Logrus87@gmail.com>
2016-05-05 12:52:29 +0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,4 +3,4 @@ Capybara.default_driver = :selenium
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
-Capybara.save_and_open_page_path = 'screenshot'
+Capybara.save_path = 'screenshot'
