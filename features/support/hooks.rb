147070af7e7b0f837b77ce456cbf97ed3c35c1b2
Aesfirius <Logrus87@gmail.com>
2016-05-05 12:29:05 +0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,4 @@
-Capybara.app_host = 'http://demo.d.educator.io'
+Capybara.app_host = 'http://google.com.ua'
 Capybara.default_driver = :selenium
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
