a29264fb20b50f775179ceb65e3b3a86ee2142f6
Aesfirius <Logrus87@gmail.com>
2016-05-05 12:21:34 +0300

    Delete hooks.rb

--- hooks.rb
+++
@@ -1,7 +0,0 @@
-Capybara.app_host = 'http://demo.d.educator.io'
-Capybara.default_driver = :selenium
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-Capybara.save_and_open_page_path = 'screenshot'
-
